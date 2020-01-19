a73cbb18037e6dce64fe8633c98b169002d11471
shtsno24 <shtsno.24@gmail.com>
2020-01-20 00:08:17 +0900

    サーボとIMUが動いた

--- IMU_Test/IMU_Test.ino
+++ IMU_Test/IMU_Test.ino
@@ -14,15 +14,17 @@
    CPOL     : 1
    CPHA     : 0
 */
-#define IMU_CS 19
+#define IMU_CS 5
 
 
-float ax, ay, az;
+float ax, ay, az, pitch, roll, yaw;
 SPIClass SPI2;
+IMU_DATA imu_data;
 
-IMU_DATA read_data(uint8_t, uint8_t);
-uint8_t read_MPU9250(uint8_t);
-void write_MPU9250(uint8_t, uint8_t);
+void read_gyro(SPIClass spi, IMU_DATA* imu_data, uint8_t cs);
+void read_acc(SPIClass spi, IMU_DATA* imu_data, uint8_t cs);
+uint8_t read_MPU9250(SPIClass spi, uint8_t reg, uint8_t cs);
+void write_MPU9250(SPIClass spi, uint8_t reg, uint8_t val, uint8_t cs);
 
 void setup() {
   Serial.begin(115200);
@@ -40,18 +42,18 @@ void setup() {
   SPI2.setFrequency(1000000); // Set SPI Clock to 1MHz
   SPI2.setDataMode(SPI_MODE3); // Set SPI Mode 3
   SPI2.setBitOrder(MSBFIRST); // Set MSB First
-  SPI2.begin(14, 12, 13, IMU_CS); //Using HSPI(SCK, MISO, MOSI, CS)
+  SPI2.begin(14, 12, 13); //Using HSPI(SCK, MISO, MOSI)
 
   // Read WHO_AM_I(@0x75)
-  uint8_t who_am_i = read_MPU9250(0x75, IMU_CS);
+  uint8_t who_am_i = read_MPU9250(SPI2, 0x75, IMU_CS);
   Serial.print("WHO_AM_I : 0x");
   Serial.println(who_am_i, HEX);
 
   // Init IMU
-  write_MPU9250(0x6B, 0x00, IMU_CS); // Disable Sleep Mode
-  write_MPU9250(0x1A, 0x00, IMU_CS); // Config(FIFO, EXT_SYNC and DLPF)
-  write_MPU9250(0x1C, 0x18, IMU_CS); // ACCL(Set Full Scale as pm16[g])
-  write_MPU9250(0x1B, 0x18, IMU_CS); // GYRO(Set Full Scale as pm2000[deg/s])
+  write_MPU9250(SPI2, 0x6B, 0x00, IMU_CS); // Disable Sleep Mode
+  write_MPU9250(SPI2, 0x1A, 0x00, IMU_CS); // Config(FIFO, EXT_SYNC and DLPF)
+  write_MPU9250(SPI2, 0x1C, 0x18, IMU_CS); // ACCL(Set Full Scale as pm16[g])
+  write_MPU9250(SPI2, 0x1B, 0x18, IMU_CS); // GYRO(Set Full Scale as pm2000[deg/s])
   Serial.println("Setup Done:");
 
   delay(1000);
@@ -64,56 +66,84 @@ void setup() {
 }
 
 void loop() {
-  IMU_DATA imu_data = read_data(IMU_CS);
-  ax = (float)(imu_data.ax_raw) / 32768.0 * 16.0;
-  ay = (float)(imu_data.ay_raw) / 32768.0 * 16.0;
-  az = (float)(imu_data.az_raw) / 32768.0 * 16.0;
+  read_acc(SPI2, &imu_data, IMU_CS);
+  delay(1);
+  read_gyro(SPI2, &imu_data, IMU_CS);
+
+  ax = (float)(imu_data.ax_raw) / 32767.0 * 16.0;
+  ay = (float)(imu_data.ay_raw) / 32767.0 * 16.0;
+  az = (float)(imu_data.az_raw) / 32767.0 * 16.0;
+
+  pitch = (float)(imu_data.pitch_raw) / 32767.0 * 2000.0;
+  roll = (float)(imu_data.roll_raw) / 32767.0 * 2000.0;
+  yaw = (float)(imu_data.yaw_raw) / 32767.0 * 2000.0;
+
   Serial.print(ax);
   Serial.print(",");
   Serial.print(ay);
   Serial.print(",");
-  Serial.println(az);
+  Serial.print(az);
+  Serial.print(",");
+  Serial.print(pitch);
+  Serial.print(",");
+  Serial.print(roll);
+  Serial.print(",");
+  Serial.println(yaw);
 }
 
-IMU_DATA read_data(uint8_t cs) {
-  uint8_t reg_acc[7] = {0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x4A, 0x00};
-  uint8_t reg_gyro[7] = {0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x80};
+void read_gyro(SPIClass spi, IMU_DATA* imu_data, uint8_t cs) {
+  uint8_t reg_gyro[7] = {0x43 | 0x80, 0x44 | 0x80, 0x45 | 0x80, 0x46 | 0x80, 0x47 | 0x80, 0x48 | 0x80, 0x00 | 0x80};
 
-  uint8_t Data_acc[7];
-  uint8_t Data_gyro[7];
-  IMU_DATA o_data;
-  digitalWrite(IMU_CS, LOW); // Start reading sensor
-  for (uint8_t i = 0; i < 7; i++) {
-    Data_acc[i] = SPI2.transfer(reg_acc[i] | 0x80); // Send register number
-  }
-  for (uint8_t i = 0; i < 7; i++) {
-    Data_gyro[i] = SPI2.transfer(reg_gyro[i] | 0x80); // Send register number
-  }
-  digitalWrite(IMU_CS, HIGH); // Stop reading sensor
-  o_data.ax_raw = Data_acc[1] << 8 | Data_acc[2];
-  o_data.ay_raw = Data_acc[3] << 8 | Data_acc[4];
-  o_data.az_raw = Data_acc[5] << 8 | Data_acc[6];
+  spi.setFrequency(20000000); // Set SPI Clock to 20MHz
+  spi.setDataMode(SPI_MODE3); // Set SPI Mode 3
+  spi.setBitOrder(MSBFIRST); // Set MSB First
+  spi.begin(14, 12, 13); //Using HSPI(SCK, MISO, MOSI)
 
-  o_data.pitch_raw = Data_gyro[1] << 8 | Data_gyro[2];
-  o_data.roll_raw = Data_gyro[3] << 8 | Data_gyro[4];
-  o_data.yaw_raw = Data_gyro[5] << 8 | Data_gyro[6];
-
-  return o_data;
-}
-
-
-uint8_t read_MPU9250(uint8_t reg, uint8_t cs) {
-  uint8_t Data;
   digitalWrite(cs, LOW); // Start reading sensor
-  SPI2.transfer(reg | 0x80); // Send register number
-  Data = SPI2.transfer(0x00); // Read reg
+  spi.transfer(reg_gyro, 7); // Send register number
   digitalWrite(cs, HIGH); // Stop reading sensor
-  return Data;
+
+  imu_data->pitch_raw = reg_gyro[1] << 8 | reg_gyro[2];
+  imu_data->roll_raw = reg_gyro[3] << 8 | reg_gyro[4];
+  imu_data->yaw_raw = reg_gyro[5] << 8 | reg_gyro[6];
 }
 
-void write_MPU9250(uint8_t reg, uint8_t val, uint8_t cs) {
+void read_acc(SPIClass spi, IMU_DATA* imu_data, uint8_t cs) {
+  uint8_t reg_acc[7] = {0x3B | 0x80, 0x3C | 0x80, 0x3D | 0x80, 0x3E | 0x80, 0x3F | 0x80, 0x40 | 0x80, 0x00 | 0x80};
+
+  spi.setFrequency(20000000); // Set SPI Clock to 20MHz
+  spi.setDataMode(SPI_MODE3); // Set SPI Mode 3
+  spi.setBitOrder(MSBFIRST); // Set MSB First
+  spi.begin(14, 12, 13); //Using HSPI(SCK, MISO, MOSI)
+
   digitalWrite(cs, LOW); // Start reading sensor
-  SPI2.transfer(reg & 0x7F); // Send register number
-  SPI2.transfer(val); // Write val
+  spi.transfer(reg_acc, 7); // Send register number
+  digitalWrite(cs, HIGH); // Stop reading sensor
+
+  imu_data->ax_raw = reg_acc[1] << 8 | reg_acc[2];
+  imu_data->ay_raw = reg_acc[3] << 8 | reg_acc[4];
+  imu_data->az_raw = reg_acc[5] << 8 | reg_acc[6];
+}
+
+
+uint8_t read_MPU9250(SPIClass spi, uint8_t reg, uint8_t cs) {
+  uint8_t Data[2] = {reg | 0x80, 0x00};
+
+  digitalWrite(cs, LOW); // Start reading sensor
+  spi.transfer(Data, 2); // Send register number
+  digitalWrite(cs, HIGH); // Stop reading sensor
+  return Data[1];
+}
+
+void write_MPU9250(SPIClass spi, uint8_t reg, uint8_t val, uint8_t cs) {
+  uint8_t reg_buff[2] = {reg & 0x7F, val};
+
+  spi.setFrequency(1000000); // Set SPI Clock to 1MHz
+  spi.setDataMode(SPI_MODE3); // Set SPI Mode 3
+  spi.setBitOrder(MSBFIRST); // Set MSB First
+  spi.begin(14, 12, 13); //Using HSPI(SCK, MISO, MOSI)
+  
+  digitalWrite(cs, LOW); // Start reading sensor
+  spi.transfer(reg_buff, 2); // Send register number
   digitalWrite(cs, HIGH); // Stop reading sensor
 }
---
+++ Servo_Test/Servo_Test.ino
@@ -0,0 +1,67 @@
+float pow2pw(float);
+uint16_t pw2cnt(float);
+
+void setup() {
+  /*
+     pulse precision : 0.01[s] / 16[bit] = 1.52587891e-7 [s/bit]
+  */
+  Serial.begin(115200);
+  ledcSetup(0, 100, 16); // Timer channel, Base clock, counter bit width
+  ledcAttachPin(32, 0); //IO pin, Timer channel
+  ledcWrite(0, 0x8000); //Timer channel, polarity change threshold count
+}
+
+void loop() {
+  for (int i = -100; i < 1; i++) {
+    ledcWrite(0, pw2cnt(pow2pw((float)i)));
+    Serial.println(pw2cnt(pow2pw((float)i)));
+    delay(1);
+  }
+  for (int i = 0; i < 100; i++) {
+    ledcWrite(0, pw2cnt(pow2pw(0.0)));
+    Serial.println(pw2cnt(pow2pw(0.0)));
+    delay(10);
+  }
+  for (int i = 0; i < 101; i++) {
+    ledcWrite(0, pw2cnt(pow2pw((float)i)));
+    Serial.println(pw2cnt(pow2pw((float)i)));
+    delay(1);
+  }
+  for (int i = 100; i > -101; i--) {
+    ledcWrite(0, pw2cnt(pow2pw((float)i)));
+    Serial.println(pw2cnt(pow2pw((float)i)));
+    delay(1);
+  }
+}
+
+float pow2pw(float power) {
+  /*
+     power must be [-100, 100]
+     Dir : 0->CW, 1->CCW
+     pulse_width is [700us, 2300us]
+  */
+  float pulse_width;
+  if (power < -100.0) {
+    power = -100.0;
+  }
+  if (power > 100.0) {
+    power = 100.0;
+  }
+
+  //  if (Dir == 0) {
+  //    power *= -1.0;
+  //  }
+
+  pulse_width = (power - (-100.0)) / (100.0 - (-100.0)) * (2300.0 - 700.0) + 700.0;
+  return pulse_width;
+}
+
+uint16_t pw2cnt(float pulse_width) {
+  /*
+      pulse_width is [700us, 2300us]
+      pulse precision : 10000[us] / 2^16 = 1.52587891e-1 [us/single_pulse_width]
+  */
+  uint16_t count;
+  count = uint16_t(pulse_width / 1.52587891e-1);
+  return count;
+}
