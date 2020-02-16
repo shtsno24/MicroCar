936c251742a6b8f927e2d6fff9e7440bb2efc463
shtsno24 <shtsno.24@gmail.com>
2020-02-16 23:17:00 +0900

    Refactor Tof_Test.ino

--- ToF_Test/ToF_Test.ino
+++ ToF_Test/ToF_Test.ino
@@ -1,74 +1,45 @@
-/* 
- * Sharp MTOF171000C0 I2C on ESP32-WROOM-32D
- * This code is based on http://akizukidenshi.com/catalog/g/gM-14538/
- * To upload this sketch to ESP32 Board, unplug IO12. 
- * Otherwise, you will not be able to upload this sketch to ESP32.
- * ESP   MTOF
- * 3.3V  VDD
- * GND   GND
- * IO12  TX1
- * NC    RX1
- * IO21  SDA 
- * IO22  SCL
- */
+/*
+   Sharp MTOF171000C0 I2C on ESP32-WROOM-32D
+   This code is based on http://akizukidenshi.com/catalog/g/gM-14538/
+
+   ESP   MTOF
+   3.3V  VDD(Red)
+   GND   GND(Black)
+   IO25  TXD/CS(White)
+   NC    RXD(Brown)
+   IO21  SDA(Yellow)
+   IO22  SCL(Green)
+*/
 
 #include <Wire.h>
 
-#define ADDRESS 0x52
+#define MTOF_ADDRESS 0x52
 
-uint16_t distance;
-uint16_t distance_tmp;
-uint8_t data_cnt;
+int32_t distance_h, distance_l, distance;
 
-void setup() 
-{
+void setup() {
   Serial.begin(115200);
-  Serial.println("MTOF171000C0_I2C");
   Wire.begin(21, 22); // SDA, SCL
-  pinMode(12, OUTPUT); // Module Select
+  pinMode(25, OUTPUT); // Module Select
   delay(1000);
 }
 
-void loop() 
-{
-  digitalWrite(12, LOW);
+void loop() {
+  digitalWrite(25, LOW);
   delay(5);
   Serial.print("distance = ");
 
-  Wire.beginTransmission(ADDRESS);
+  Wire.beginTransmission(MTOF_ADDRESS);
   Wire.write(0xD3);
   Wire.endTransmission(false);
-  Wire.requestFrom(ADDRESS, 2);
-  data_cnt = 0;
-  distance = 0;
-  distance_tmp = 0;
-  while(Wire.available())
-  {
-    distance_tmp = Wire.read();
-    distance = (distance << (data_cnt*8)) | distance_tmp;
-    data_cnt++;
-  }
+  Wire.requestFrom(MTOF_ADDRESS, 2);
+
+  distance_h = Wire.read();
+  distance_l = Wire.read();
+  distance = (distance_h << 8) | distance_l;
 
   Serial.print(distance);
   Serial.println(" mm");
-  digitalWrite(12, HIGH);
-  delay(45);
-}
-
-int readDistance(byte reg)
-{
-  Wire.beginTransmission(ADDRESS);
-  Wire.write(reg);
-  Wire.endTransmission(false);
-  Wire.requestFrom(ADDRESS, 2);
-  data_cnt = 0;
-  distance = 0;
-  distance_tmp = 0;
-  while(Wire.available())
-  {
-    distance_tmp = Wire.read();
-    distance = (distance << (data_cnt*8)) | distance_tmp;
-    data_cnt++;
-  }
-  return distance;
+  digitalWrite(25, HIGH);
+//  delay(30);
 }
