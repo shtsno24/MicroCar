8977059cc30de9f46407ed38f712aca88b3290fc
shtsno24 <shtsno.24@gmail.com>
2020-07-24 12:28:31 +0900

    Add Line Graph Class

--- Matplotlib/view_test.py
+++ Matplotlib/view_test.py
@@ -1,42 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
-import view_agent
-
-class BaseAgent:
-
-    # jvec = None
-    # avec = None
-    # vvec = None
-    # pvec = None
-
-    # jvec_old = None
-    # avec_old = None
-    # vvec_old = None
-    # pvec_old = None
-
-    # ts = None  # [s]
-
-    def __init__(self,
-                 jvec=np.zeros(3, dtype=np.float32),
-                 avec=np.zeros(3, dtype=np.float32),
-                 vvec=np.zeros(3, dtype=np.float32),
-                 pvec=np.zeros(3, dtype=np.float32),
-                 ts=0.001):
-
-        self.jvec = jvec
-        self.avec = avec
-        self.vvec = vvec
-        self.pvec = pvec
-        self.ts = ts
-        self.jvec_old = np.copy(self.jvec)
-        self.avec_old = np.copy(self.avec)
-        self.vvec_old = np.copy(self.vvec)
-        self.pvec_old = np.copy(self.pvec)
+import base_agent
+import plotter
 
 
-class SteerAgent(BaseAgent):
+class SteerAgent(base_agent.BaseAgent):
     def __init__(self,
                  steer_angle=0.0,
                  jvec=np.zeros(3, dtype=np.float32),
@@ -68,16 +37,22 @@ class SteerAgent(BaseAgent):
 
 
 if __name__ == "__main__":
-    plot_view = view_agent.ViewAgent(limit=200.0, len_points=1000, icon_radius=10.0)
+    fig = plt.figure()
+    pos_ax = fig.add_subplot(2, 1, 1)
+    line_ax = fig.add_subplot(2, 1, 2)
+    plot_view = plotter.Scatter(fig, pos_ax, plot_area=(200.0, 200.0), len_points=200, show_icon=True, icon_radius=10.0)
+    line_view = plotter.Line(fig, line_ax, plot_area=(12000, 1), len_points=12000)
     agent = SteerAgent()
+    update_func = lambda x, y, z: z + (x + y) * agent.ts / 2.0
 
     input(">>")
     for i in range(12000):
-        agent.update_jerk(np.array([np.cos(i / 4000.0) + (np.random.randn() / 50.0),
-                                    np.sin(i / 4000.0) + (np.random.randn() / 50.0),
+        agent.update_jerk(np.array([np.cos(i / 4000.0),
+                                    np.sin(i / 4000.0),
                                     np.sin(i / 4000.0)]))
-        agent.update_acc(lambda x, y, z: z + (x + y) * agent.ts / 2.0)
-        agent.update_vel(lambda x, y, z: z + (x + y) * agent.ts / 2.0)
-        agent.update_pos(lambda x, y, z: z + (x + y) * agent.ts / 2.0)
+        agent.update_acc(update_func)
+        agent.update_vel(update_func)
+        agent.update_pos(update_func)
         plot_view.plot(agent.pvec[:2], agent.pvec[2])
+        line_view.plot(agent.avec[0])
     input(">>")
